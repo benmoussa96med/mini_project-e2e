63de3928f1f751b38973d2ed19ae0cb0e2cff083
Mohamed <benmoussa96med@gmail.com>
2021-02-04 14:44:11 +0100

    Create person.util.js

---
+++ e2e/utils/person.util.js
@@ -0,0 +1,17 @@
+const Chance = require('chance');
+const chanceObj = new Chance();
+
+export function generatPerson() {
+    const firstName = chanceObj.last().toLowerCase();
+    const lastName = chanceObj.first().toLowerCase();
+    const rand = Math.ceil(Math.random() * 100);
+    const email = firstName + rand + lastName + '@gmail.com';
+    const password = 'testing';
+    return {
+        firstName: firstName,
+        lastname: lastName,
+        email: email,
+        password: password
+
+    };
+}
