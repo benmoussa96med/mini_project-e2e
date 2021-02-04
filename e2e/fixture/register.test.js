ee8284d99b21b9526404d9061345569ea2bb581b
Mohamed <benmoussa96med@gmail.com>
2021-02-04 14:44:00 +0100

    Create register.test.js

---
+++ e2e/fixture/register.test.js
@@ -0,0 +1,22 @@
+import { baseUrl } from '../utils/constant.util';
+import { RegisterElements } from '../page-objects/register.po';
+import { generatPerson } from '../utils/person.util';
+
+
+fixture('Welcome register').page(baseUrl);
+
+test('welcome for register new user', async t => {
+    //Arrnage
+    const person = generatPerson();
+    //Act
+    await t
+        .debug()
+        .typeText(RegisterElements.email, person.email)
+        .typeText(RegisterElements.password, person.password)
+        .typeText(RegisterElements.firstName, person.firstName)
+        .typeText(RegisterElements.lastName, person.lastname)
+        .click(RegisterElements.btnRegister);
+    //Asset
+    await t.expect(RegisterElements.msgAccept.innerText).contains(person.firstName);
+    await t.expect(RegisterElements.msgAccept1.exists).ok()
+})
