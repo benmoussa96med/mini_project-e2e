e7acc3cf0d0915d6dde93aae1761f1ff6dd45738
Mohamed <benmoussa96med@gmail.com>
2021-02-05 17:16:45 +0100

    Update login.po.js

--- e2e/page-objects/login.po.js
+++ e2e/page-objects/login.po.js
@@ -1,13 +1,19 @@
 import { Selector } from 'testcafe';
+
 const selectQuest = Selector('select.x-created');
 const selectOpt = selectQuest.find('option');
+
 export const generatLogin = {
     userName: Selector('#okta-signin-username'),
     password: Selector('#okta-signin-password'),
     checkRemember: Selector('label').withText('Remember me'),
     singIn: Selector('#okta-signin-submit'),
-    selectQuestion: selectQuest,
-    selectOption: selectOpt,
-    replyAnswer: Selector('input.x-created')
 
+
+    customQuestion: Selector('input.x-created'),
+
+    // customQuestion: Selector('input.x-created'),
+
+    selectQuestion: selectQuest,
+    selectOption: selectOpt
 };
