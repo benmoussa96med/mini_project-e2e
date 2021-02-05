675e17607655e0a9556e8a3164a2dde68b1b3e14
Mohamed <benmoussa96med@gmail.com>
2021-02-05 11:01:24 +0100

    Update login.test.js

--- e2e/fixture/login.test.js
+++ e2e/fixture/login.test.js
@@ -1,11 +1,18 @@
-fixture('Welcome login').page(baseUrl);
+import { loginUrl, password } from '../utils/constant.util';
+import { generatLogin } from '../page-objects/login.po';
+fixture('Welcome login').page(loginUrl);
 
 test('welcome for connecte user', async t => {
     //Arrnage
-
+    const email = 'donnelly85harry@gmail.com';
     //Act
     await t
-
-    //Asset
+        .debug()
+        .typeText(generatLogin.userName, email)
+        .typeText(generatLogin.password, password)
+        .click(generatLogin.checkRemember)
+        .click(generatLogin.singIn)
+        .wait(4000)
+        //Asset
 
 })
