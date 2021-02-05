f6c8335e4b5006da07e16c8cef1a9a5a495e1dd4
Mohamed <benmoussa96med@gmail.com>
2021-02-05 11:01:42 +0100

    Update constant.util.js

--- e2e/utils/constant.util.js
+++ e2e/utils/constant.util.js
@@ -1,2 +1,3 @@
 export const baseUrl = 'https://dev-842917.oktapreview.com/signin/register';
+export const loginUrl = 'https://dev-842917.oktapreview.com/signin/';
 export const password = 'testing';
